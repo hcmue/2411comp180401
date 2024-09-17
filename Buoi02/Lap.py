c57912c0741ad709db2f5dbe8af04eb78db708c3
HIENLTH @ HCMUE <hyhien@gmail.com>
2024-09-17 07:32:27 +0700

    Demo range

---
+++ Buoi02/Lap.py
@@ -0,0 +1,12 @@
+# Demo range
+for item in range(11): # 0 --> 10
+    print(item)
+
+for item in range(2, 21): # 2 --> 20
+    print(item)
+
+for item in range(2, 23, 3): # 2, 5, 8, 11, 14, 17, 20
+    print(item, end=", ")
+print()
+
+print("Python", "Programming", 2024, sep="/")
---
+++ Buoi02/README.md
@@ -0,0 +1,7 @@
+# range(start, end, step)
+
+- range(n): 0 ==> n - 1
+- range(x, y): x ==> y - 1
+- range(x, y, 3): x < y - 1, mỗi phần tử tăng 3
+
+# print(v1,v2,v3, sep=" ", end="\n")
