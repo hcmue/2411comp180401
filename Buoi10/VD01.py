33560f8df502b06c37351515d81f259cc29aea78
HIENLTH @ HCMUE <hyhien@gmail.com>
2024-11-26 07:44:22 +0700

    First demo Signed-off-by: HIENLTH @ HCMUE <hyhien@gmail.com>

--- Buoi10/VD01.py
+++ Buoi10/VD01.py
@@ -1,21 +1,18 @@
-# Buổi 10 (26/11/2024)
-
-Template vẽ
-
-```
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Định nghĩa hàm vẽ
-
+x = np.arange(0, 5 * np.pi, 0.01)
+y = np.cos(x)
 
 # Vẽ
-plt.plot()
+plt.plot(x, y)
 
 # Custom: tiêu đề, label trục
 plt.xlabel = "Trục X"
 plt.ylabel = "Trục Y"
 plt.title = "Bảng giá vàng"
+plt.legend(['Cos(x)'])
 
 # Hiển thị lên màn hình
 plt.show()
-```
